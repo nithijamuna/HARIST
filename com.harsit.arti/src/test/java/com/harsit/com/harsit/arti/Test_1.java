ee95455125a9003d958ce651dc16af9e032ac8db
Arulventhan <arulv6@gmail.com>
2019-02-24 08:57:39 +0530

    first commit

---
+++ com.harsit.arti/src/test/java/com/harsit/com/harsit/arti/Test_1.java
@@ -0,0 +1,13 @@
+package com.harsit.com.harsit.arti;
+
+import org.testng.annotations.Test;
+
+public class Test_1
+{
+	@Test
+	public static void Arul()
+	{
+		System.out.println("Welcome Arulventhan");
+		
+	}
+}

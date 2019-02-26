bca899a0f2975160c899099ed13670d18419b5d2
Arulventhan <arulv6@gmail.com>
2019-02-26 08:29:00 +0530

    first commit

--- com.harsit.arti/src/test/java/com/harsit/com/harsit/arti/Test_1.java
+++ com.harsit.arti/src/test/java/com/harsit/com/harsit/arti/Test_1.java
@@ -1,14 +1,28 @@
 package com.harsit.com.harsit.arti;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class Test_1
 {
 	@Test
 	public static void Arul()
+	
+	
 	{
-		System.out.println("Welcome Arulventhan and nithya and meera and sidthan");
-		System.out.println("Punitha and harsitha");
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Arul\\Downloads\\geckodriver-v0.24.0-win64\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys("arul");
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		driver.close();
+				
 		
 	}
 }
